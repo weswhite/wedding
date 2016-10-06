8b1c8320a4d785da024da1b4ec4420f8b00bff28
Travers Pinkerton <jtpinkerton@gmail.com>
2016-10-06 18:00:39 -0500

    server in progress

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
-app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-	res.send('<html> <body>HIYA!</body> </html>')
-})
+//middleware
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '../frontend/src/'));
+
+// app.get('/', function(req, res){
+// 	res.sendFile(path.join(__dirname + '/index.html'));
+// })
 
 // var port = process.env.PORT || 8080;
 app.listen(8080, function() { 
