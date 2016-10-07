daeaeb117df20a34e4c12f5714f3150a4e27694d
Wes White <wtealwhite@gmail.com>
2016-10-07 10:10:20 -0500

    restructure for express :(

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var compression = require('compression');
 
 var app = express();
 
 //middleware
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '../frontend/src/'));
+app.use(compression());
+app.use(express.static(__dirname + '/app/'));
 
 // app.get('/', function(req, res){
 // 	res.sendFile(path.join(__dirname + '/index.html'));
 // })
 
 // var port = process.env.PORT || 8080;
-app.listen(8080, function() { 
+app.listen(8080, function() {
 	console.log('Wedding RSVP listening on localhost:8080');
 });
